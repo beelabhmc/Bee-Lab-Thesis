fde29a959ee2c565924a589233f882cc5c653e71
swoodman213 <swoodman213@gmail.com>
2016-02-23 15:41:53 -0800

    Nectar quantity done
    Have calculations for nectar/patch-questions with some being 0s, does
    that reflect no nectar or just that we can’t get it?

--- Flower Data /Flower Nectar Quantity.R
+++ Flower Data /Flower Nectar Quantity.R
@@ -5,7 +5,7 @@ library(dplyr)
 setwd("~/Google Drive/Semester 8/Thesis/NetLogo+R GitHub/Flower Data ")
 
 file.names <- c("Plant.ID", "Species.Name", "Date", "Flow.Diam.mm", "Cor.Len.mm", 
-                "Florets", "Total.Vol.Nec.micro.L", "Vol.Flor.micro.L", "BRIX", 
+                "Florets", "Total.Vol.Nec.micro.L", "Vol.Flwr.micro.L", "BRIX", 
                 "Sugar.micro.g", "Sugar.Flor.micro.g", "Notes")
 flower.char <- read.csv("Flower Characteristics.csv", header = TRUE, col.names = file.names)
 bfs.data <- read.csv("BFS Master Survey.csv", header = TRUE)
@@ -25,9 +25,20 @@ species.names.all <- sort(unique(as.character(bfs.data$Species.Name)))
 
 bfs.data <- filter(bfs.data, bfs.data$Species.Name %in% species.names.bfs)
 
-# Nectar amount calculations
-avg.nectar <- sapply(species.names.bfs, function(i) mean(bfs.data[bfs.data$Species.Name == i,]$X.Flowers, na.rm = TRUE))
-# The averages in avg.volume are for a 2m x 1m plot
-# Thus, multiply by (6.67 x 6.67)/2 to get μL found in one NetLogo patch
-avg.nectar.per.patch <- avg.nectar * ((6.67^2)/2)
-avg.vol.patch
+# Flower counts 
+avg.flwr <- sapply(species.names.bfs, 
+                   function(i) mean(bfs.data[bfs.data$Species.Name == i,]$X.Flowers, 
+                                    na.rm = TRUE))
+# The averages in avg.flwr are for a 2m x 1m plot
+# Thus, multiply by (6.67 x 6.67)/2 to get flowers found in one NetLogo patch
+avg.flwr.per.patch <- avg.flwr * ((6.67^2)/2)
+avg.flwr.per.patch
+
+# Avg amount of nectar per flower for each species
+avg.nectar.per.flwr <- sapply(species.names.bfs, 
+                              function(j) mean(flower.char[flower.char$Species.Name == j,]$Vol.Flwr.micro.L, 
+                                               na.rm = TRUE))
+
+avg.nectar.per.patch <- avg.flwr.per.patch * avg.nectar.per.flwr
+avg.nectar.per.patch
+avg.nectar.per.patch/25
