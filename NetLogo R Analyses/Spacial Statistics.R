efedf6eafa9aace70866d73b6e38e8b1c080f84b
swoodman213 <swoodman213@gmail.com>
2016-02-28 21:26:42 -0800

    Sorted R dense testing. Spat Stats includes dense analysis

--- NetLogo R Analyses/Spacial Statistics.R
+++ NetLogo R Analyses/Spacial Statistics.R
@@ -1,23 +1,45 @@
 # Sam Woodman
 # Analysis of spacial statistic R
+# General, Desne, and Sparse Specific Testing
 
 library(dplyr)
 setwd("~/Google Drive/Semester 8/Thesis/NetLogo+R GitHub/BehaviorSpace Data/R Value/")
-r.data <- read.csv("Bees R values-table.csv", header = TRUE, skip = 6)[,c(2,3,4,5,8)]
+
+### General Testing
+g.data <- read.csv("Bees R values-table.csv", header = TRUE, skip = 6)[,c(2,3,4,5,8)]
 
 ## Filter data by dense and sparse
-r.data <- r.data %>% filter(c1_mult >= c2_mult)
-r.data.dense <- filter(r.data, r.data$resource_density == "\"dense\"")
-r.data.dense.r <- r.data.dense$R
-r.data.sparse <- filter(r.data, r.data$resource_density == "\"sparse\"")
-r.data.sparse.r <- r.data.sparse$R
+g.data <- g.data %>% filter(c1_mult >= c2_mult)
+g.data.dense <- filter(g.data, g.data$resource_density == "\"dense\"")
+g.data.dense.r <- g.data.dense$R
+g.data.sparse <- filter(g.data, g.data$resource_density == "\"sparse\"")
+g.data.sparse.r <- g.data.sparse$R
 
 ## Divide data into groups .03 away from: R = 0.4, 0.6, 0.8, 1.0
 # Dense
-r.data.dense.4 <- r.data.dense %>% filter(abs(R - 0.4) <= 0.03)
-r.data.dense.6 <- r.data.dense %>% filter(abs(R - 0.6) <= 0.03)
-r.data.dense.8 <- r.data.dense %>% filter(abs(R - 0.8) <= 0.03)
+g.data.dense.4 <- g.data.dense %>% filter(abs(R - 0.4) <= 0.03)
+g.data.dense.6 <- g.data.dense %>% filter(abs(R - 0.6) <= 0.03)
+g.data.dense.8 <- g.data.dense %>% filter(abs(R - 0.8) <= 0.03)
 # Sparse
-r.data.sparse.4 <- r.data.sparse %>% filter(abs(R - 0.4) <= 0.03)
-r.data.sparse.6 <- r.data.sparse %>% filter(abs(R - 0.6) <= 0.03)
-r.data.sparse.8 <- r.data.sparse %>% filter(abs(R - 0.8) <= 0.03)
+g.data.sparse.4 <- g.data.sparse %>% filter(abs(R - 0.4) <= 0.03)
+g.data.sparse.6 <- g.data.sparse %>% filter(abs(R - 0.6) <= 0.03)
+g.data.sparse.8 <- g.data.sparse %>% filter(abs(R - 0.8) <= 0.03)
+
+
+### Dense Testing
+d.data <- read.csv("Bees R dense testing-table.csv", header = TRUE, skip = 6)[-c(6,7,9)]
+
+## Divide data into groups .01 away from: R = 0.4, 0.6, 0.8
+d.data.4 <- d.data %>% filter(abs(R - 0.4) <= 0.01)
+temp.4 <- which.max(table(floor(d.data.4$X.run.number./20))) # Best sequence is d.data[2740:2759,]
+idx.4 <- 20 * as.numeric(names(temp.4))
+# 
+
+d.data.6 <- d.data %>% filter(abs(R - 0.6) <= 0.01)
+temp.6 <- which.max(table(floor(d.data.6$X.run.number./20))) # Best sequence is d.data[280:299,] d.data[300:319,]
+idx.6 <- 20 * as.numeric(names(temp.6))
+
+d.data.8 <- d.data %>% filter(abs(R - 0.8) <= 0.01)
+temp.8 <- which.max(table(floor(d.data.8$X.run.number./20))) # Best sequence is d.data[820:839,]
+idx.8 <- 20 * as.numeric(names(temp.8))
+
